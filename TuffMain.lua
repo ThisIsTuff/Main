4916d4b26cc2a9adb19ef56617178d3698a6783b
ThisIsTuff <138639410+ThisIsTuff@users.noreply.github.com>
2024-07-04 14:32:30 +0100

    Update TuffMain.lua

--- TuffMain.lua
+++ TuffMain.lua
@@ -1,13 +1,9 @@
 local StuffYeah = {
-    [14400545236] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/ClickSimulator.lua', -- Click Sim (game died)
-    [15327837196] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/ClickSimulator.lua', -- Click Sim second world (game died)
-    [13127800756] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/ArmWrestleSim.lua', -- AWS
-    [14236123211] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/ArmWrestleSim.lua', --PUNCH SIM (game died)
-    [14433762945] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/AnimeChampions.lua', -- ANIME CHAMPIONS (game died)
-    [12886143095] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/AnimeLastStand.lua', -- ANIME LAST STAND
-    [12900046592] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/AnimeLastStand.lua', -- ANIME LAST STAND ( INGAME)
-    [15050509770] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/SysphusSimulator.lua', -- sus (sisyphus) simulator
-    [15444929588] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/SkibidiTowerDefense.lua', -- skibi tower defense (why did i make this?)
-    [16158315163] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/SkibidiTowerDefense.lua' -- 
+    [32004490] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/ArmWrestleSim.lua',
+    [34287998] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/GymLeague.lua',
+    [12229756] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/AnimeLastStand.lua',
+    [33397850] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/SkibidiTowerDefense.lua',
+    [33243447] = 'https://raw.githubusercontent.com/ThisIsTuff/ArmWrestleSim/main/SysphusSimulator.lua'
+    
 }
-loadstring(game:HttpGet(StuffYeah[game.PlaceId]))()
+loadstring(game:HttpGet(StuffYeah[game.CreatorId]))()
